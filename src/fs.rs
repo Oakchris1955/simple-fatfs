983350de7aa3248cc2c1ce0d00fd404e8d777da5
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2024-08-07 14:57:39 +0300

    docs: make 2 consts public & document them

--- src/fs.rs
+++ src/fs.rs
@@ -22,8 +22,10 @@ use time::{Date, PrimitiveDateTime, Time};
 
 use crate::{error::*, io::prelude::*, path::PathBuf};
 
-const SECTOR_SIZE_MIN: usize = 512;
-const SECTOR_SIZE_MAX: usize = 4096;
+/// The minimum size (in bytes) a sector is allowed to have
+pub const SECTOR_SIZE_MIN: usize = 512;
+/// The maximum size (in bytes) a sector is allowed to have
+pub const SECTOR_SIZE_MAX: usize = 4096;
 
 /// Place this in the BPB _jmpboot field to hang if a computer attempts to boot this partition
 /// The first two bytes jump to 0 on all bit modes and the third byte is just a NOP
