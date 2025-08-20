38f216bf7c897ec90e475ceab61e14fe717adad1
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2025-08-20 15:19:03 +0300

    chore: use `setbits` crate

--- src/utils/bits.rs
+++ src/utils/bits.rs
@@ -1,7 +1,1 @@
-/// Sets the low `n` bits of a [`u32`] to `1`
-///
-/// Original code taken from:
-/// <https://users.rust-lang.org/t/how-to-make-an-integer-with-n-bits-set-without-overflow/63078/3>
-pub(crate) fn setbits_u32(n: u8) -> u32 {
-    u32::MAX >> (u32::BITS - u32::from(n))
-}
+pub(crate) use setbits::*;
