8a4d2f82c2b4dfcb81276dc0a32fa675b4bfb1fd
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2025-09-21 16:00:31 +0300

    chore: revert/replace "chore: suppress warnings"
    This reverts commit a157b411527cadd11b4f4c6ad0735e01356d0c88.
    
    It also replaces underscored variables with #[expect(...)] lints

--- src/fat/consts.rs
+++ src/fat/consts.rs
@@ -5,5 +5,5 @@ pub const MAX_SECTOR_SIZE: usize = 4096;
 
 /// Place this in the BPB _jmpboot field to hang if a computer attempts to boot this partition
 /// The first two bytes jump to 0 on all bit modes and the third byte is just a NOP
-#[allow(dead_code)]
+#[expect(dead_code)]
 pub(crate) const INFINITE_LOOP: [u8; 3] = [0xEB, 0xFE, 0x90];
