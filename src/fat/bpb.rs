9286961871ac0c9af9e84e099176f8a52d981085
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2025-09-21 16:00:31 +0300

    chore: revert/replace "chore: suppress warnings"
    This reverts commit a157b411527cadd11b4f4c6ad0735e01356d0c88.
    
    It also replaces underscored variables with #[expect(...)] lints

--- src/fat/bpb.rs
+++ src/fat/bpb.rs
@@ -7,7 +7,7 @@ use bitfield_struct::bitfield;
 #[allow(clippy::large_enum_variant)]
 pub(crate) enum BootRecord {
     Fat(BootRecordFAT),
-    #[allow(dead_code)]
+    #[expect(dead_code)]
     ExFAT(BootRecordExFAT),
 }
 
@@ -28,12 +28,12 @@ impl BootRecord {
                 SectorIndex::from(boot_record_fat.first_fat_sector())
                     + SectorIndex::from(n) * boot_record_fat.fat_sector_size()
             }
-            #[allow(unreachable_code)]
-            BootRecord::ExFAT(_boot_record_exfat) => {
+            #[expect(unused_variables, unreachable_code)]
+            BootRecord::ExFAT(boot_record_exfat) => {
                 // this should work, but ExFAT is not yet implemented, so...
                 todo!("ExFAT not yet implemented");
-                SectorIndex::from(_boot_record_exfat.fat_count)
-                    + SectorIndex::from(n) * _boot_record_exfat.fat_len
+                SectorIndex::from(boot_record_exfat.fat_count)
+                    + SectorIndex::from(n) * boot_record_exfat.fat_len
             }
         }
     }
@@ -127,9 +127,9 @@ impl BootRecordFAT {
     #[inline]
     /// The FAT type of this file system
     pub(crate) fn fat_type(&self) -> FATType {
+        #[expect(unreachable_code)]
         if self.bpb.bytes_per_sector == 0 {
             todo!("ExFAT not yet implemented");
-            #[allow(unreachable_code)]
             FATType::ExFAT
         } else {
             let total_clusters = self.total_clusters();
@@ -151,20 +151,30 @@ pub(crate) struct BootRecordExFAT {
     pub _oem_identifier: [u8; 8],
     pub _zeroed: [u8; 53],
     pub _partition_offset: u64,
-    pub _volume_len: u64,
-    pub _fat_offset: u32,
+    #[expect(dead_code)]
+    pub volume_len: u64,
+    #[expect(dead_code)]
+    pub fat_offset: u32,
     pub fat_len: u32,
-    pub _cluster_heap_offset: u32,
-    pub _cluster_count: u32,
-    pub _root_dir_cluster: u32,
-    pub _partition_serial_num: u32,
-    pub _fs_revision: u16,
-    pub _flags: u16,
+    #[expect(dead_code)]
+    pub cluster_heap_offset: u32,
+    #[expect(dead_code)]
+    pub cluster_count: u32,
+    #[expect(dead_code)]
+    pub root_dir_cluster: u32,
+    #[expect(dead_code)]
+    pub partition_serial_num: u32,
+    #[expect(dead_code)]
+    pub fs_revision: u16,
+    #[expect(dead_code)]
+    pub flags: u16,
     pub sector_shift: u8,
     pub cluster_shift: u8,
     pub fat_count: u8,
-    pub _drive_select: u8,
-    pub _used_percentage: u8,
+    #[expect(dead_code)]
+    pub drive_select: u8,
+    #[expect(dead_code)]
+    pub used_percentage: u8,
     pub _reserved: [u8; 7],
 }
 
