5e7b3217e9f651cc6bd73bd80ee301c38165e5e2
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2026-01-17 16:21:39 +0200

    doc: add note regarding volume labels

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,18 @@
 //! }
 //! ```
 //!
+//! ## Notes
+//!
+//! - **Regarding volume labels**
+//!
+//!   For historic reasons FAT label is stored in two different
+//!   locations: in the boot sector and as a special volume label entry
+//!   in the root directory. Windows read the FAT label only from the root
+//!   directory (and only update that). Keep that in mind when using the
+//!   various volume label functions
+//!
+//!   For more info, please check <https://man7.org/linux/man-pages/man8/fatlabel.8.html>
+//!
 //! [`Read`]: io::Read
 //! [`Seek`]: io::Seek
 //! [`Write`]: io::Write
